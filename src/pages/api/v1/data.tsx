074adc8627042ea96755cd07ccf54809692cb9c3
Daniel Rauhut <rauhut@publicplan.de>
2023-06-23 21:34:37 +0200

    remove CORS middleware, we don't need it here.

--- src/pages/api/v1/data.tsx
+++ src/pages/api/v1/data.tsx
@@ -1,17 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import NextCors from 'nextjs-cors';
 import path from 'path';
 import fs from 'fs';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  //? CORS is overkill as we fetch serverside, but why not
-  await NextCors(req, res, {
-    methods: ['GET'],
-    origin: '*',
-    credentials: true,
-    optionsSuccessStatus: 200,
-  });
-
   if (req.method !== 'GET') {
     res.setHeader('Allow', ['GET']);
     return res.status(405).send(`Method ${req.method} is not allowed`);
