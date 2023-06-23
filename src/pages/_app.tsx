d1dd5022f7c9d0dd94f0fbb00061877d93ab4670
Daniel Rauhut <rauhut@publicplan.de>
2023-06-23 15:25:39 +0200

    dynamic notification position between mobile and desktop view

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { CartProvider } from '@/components/ShoppingCart/CartContext';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, ToastPosition } from 'react-toastify';
 import '@/styles/globals.css';
 import 'react-toastify/dist/ReactToastify.css';
 import type { AppProps } from 'next/app';
@@ -7,14 +7,29 @@ import { useEffect, useState } from 'react';
 import Header from '@/components/Header/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [notificationPosition, setNotificationPosition] = useState<ToastPosition>();
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 600) setNotificationPosition('bottom-center');
+      else setNotificationPosition('top-right');
+    };
+
+    setNotificationPosition(window.innerWidth < 600 ? 'bottom-center' : 'top-right');
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <CartProvider>
       <div className="flex flex-col min-h-screen">
         <Header />
-        <div className="flex-1">
+        <div className="flex-1 flex">
           <Component {...pageProps} />
         </div>
-        <ToastContainer position={'bottom-center'} />
+        <ToastContainer position={notificationPosition} />
         {/* <Footer /> */}
       </div>
     </CartProvider>
